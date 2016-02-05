7afb7d1a10903b3082d03315adcc362e34e03a77
bbluue IMac <bbluueImac@imac-de-bbluue.local>
2016-02-05 09:44:15 -0500

    Initial Commit

---
+++ bbluue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bbluue
+//
+//  Created by bbluue IMac on 2/5/16.
+//  Copyright © 2016 bbluue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bbluue/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  bbluue
+//
+//  Created by bbluue IMac on 2/5/16.
+//  Copyright © 2016 bbluue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
